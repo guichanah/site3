21407ed6814823f1037eba1f703f94bd0555716b
guichanah <dbs0467@gmail.com>
2025-01-13 12:45:18 +0900

    0113

--- js/script.js
+++ js/script.js
@@ -20,3 +20,16 @@ var mainvisual = new Swiper("#mainvisual", {
         bulletActiveClass:"active" //활성화된 페이지버튼의 클래스
     },
 });
+
+/*press 스와이퍼 호출*/
+var press = new Swiper("#press .inner",{
+    wrapperClass:"slider",
+    slideClass:"item",
+    slidesPerView:5,
+    spaceBetween:30,
+    pagination: { //페이지버튼 설정
+        el:'.dot', //페이지버튼 영역 지정
+        clickable:true, //클릭시 슬라이드 전환기능 활성화
+        bulletActiveClass:"active" //활성화된 페이지버튼의 클래스
+    },
+});
